607084aa4d3c2daabf8f9c05c8a43749b5b57fc6
Michael Primavera <mr.michael.primavera@outlook.com>
2026-03-22 20:17:18 -0400

    updated the romans file for cleaner solution

--- romans.cpp
+++ romans.cpp
@@ -1,52 +1,53 @@
 #include <iostream>
-#include <unordered_map>
-
 using namespace std;
 
 class Solution {
     public:
-        int romanToInt(string s) {
-            unordered_map <char, int> romans = 
-            {
-                {'I', 1},
-                {'V', 5},
-                {'X', 10},
-                {'L', 50},
-                {'C', 100},
-                {'D', 500},
-                {'M', 1000},
-            };
-
-            int decimal = 0;
-            int length = s.length() - 1;
-
-            for (int i = 0; i <= length; i++)
-            {
-                int value = romans[s[i]];
-
-                if ((i != length) && (romans[s[i+1]] > value))
-                {
-                    decimal += (romans[s[i+1]] - value);
-                    i++;
-                }
-
-                else
-                {
-                    decimal += value;
-                }                
-            }
-
-            return decimal;
+    int decimal(char c)
+    {
+        switch(c)
+        {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
         }
+    }
 
+    int romanToInt(string romans)
+    {
+        int result = 0;
+        int len = romans.length();
+
+        for (int i = 0; i < len; i++)
+        {
+            int deciml = decimal(romans[i]);
+
+            if (i < (len-1))
+            {
+                int next_deciml = decimal(romans[i + 1]);
+
+                if (deciml < next_deciml)
+                {
+                    result -= deciml;
+                }
+                else result += deciml;
+            }
+            else result += deciml;
+        }
+        return result;
+    }
 };
 
-int main()
+int main ()
 {
     Solution sol;
 
-    std::cout << sol.romanToInt("MCMXCIV") << std::endl;
-
+    cout << sol.romanToInt("LVIII") << endl;
 
     return 0;
 }
