2941c18489bfe924aa5b55dbb5f2b46225d7b824
Michael Primavera <mr.michael.primavera@outlook.com>
2026-03-30 16:09:28 -0400

    added a file for pointer to an object

---
+++ pointer_to_object.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+    public:
+    int length;
+    int breadth;
+
+    int area()
+    {
+        return length * breadth;
+    }
+
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+
+
+
+int main()
+{
+    // create a Rectangle object called r
+    Rectangle r1;
+    r1.length = 10;
+    r1.breadth = 5;
+
+    // create a Rectangle object in heap and access with Rectange pointer
+    Rectangle* ptr1 = new Rectangle;
+
+    // set the heap rectangle length to 5 with the pointer accessor operator
+    ptr1->length = 10;
+
+    // set r1's breadth to 5 using the pointer accessor operator
+    ptr1->breadth = 5;
+
+    bool equal = (r1.length == ptr1->length);
+
+    // both accessor operators produce the same value / or data attribute
+    cout << equal << endl << r1.length << " = " << ptr1->length << endl << ptr1;
+
+    // both accessor operators produce the same function of the same object instance
+    cout << endl << r1.area() << " = " << ptr1->area() << endl << r1.perimeter() << " = " << ptr1->perimeter() << endl;
+    return 0;
+}
